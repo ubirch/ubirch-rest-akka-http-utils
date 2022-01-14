e540abece9bf036990326bbc2d2f21e4504ed98b
Rueger <lotta.rueger@ubirch.com>
2022-01-14 18:18:20 +0100

    upgrading - scala to 2.13.6 - scala-test to 3.2.10 - akka to 2.6.18 - akka-http to 10.2.6 - akka-http-cors to 1.1.2

--- src/main/scala/com/ubirch/util/rest/akka/test/cors/CORSUtil.scala
+++ src/main/scala/com/ubirch/util/rest/akka/test/cors/CORSUtil.scala
@@ -2,13 +2,14 @@ package com.ubirch.util.rest.akka.test.cors
 
 import akka.http.scaladsl.model.headers.{HttpOriginRange, `Access-Control-Allow-Credentials`}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import org.scalatest.{FeatureSpec, Matchers}
+import org.scalatest.featurespec.AnyFeatureSpec
+import org.scalatest.matchers.should.Matchers
 
 /**
   * author: cvandrei
   * since: 2016-11-17
   */
-trait CORSUtil extends FeatureSpec
+trait CORSUtil extends AnyFeatureSpec
   with Matchers
   with ScalatestRouteTest {
 
@@ -16,7 +17,7 @@ trait CORSUtil extends FeatureSpec
 
     if (exist) {
 
-      header("Access-Control-Allow-Origin") should be('isDefined)
+      header("Access-Control-Allow-Origin").isDefined shouldBe true
       header("Access-Control-Allow-Credentials") should be(Some(`Access-Control-Allow-Credentials`(true)))
 
     } else {
